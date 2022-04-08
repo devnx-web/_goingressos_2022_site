b82ee6d02a2c97556c375153e608c3ed5a13194e
fabiodevn1 <40078286+fabiodevn1@users.noreply.github.com>
2022-04-08 16:34:07 -0300

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  beforeMount() {
+        this.$router.push(`/we-are-carnaval`)
+  },
+}
+</script>
